3d33814681506af4b2b68fc1d74cf91d2299e21d
reliaasp <aurelsalsabiila279@gmail.com>
2026-04-15 00:43:33 +0700

    Update login.php

--- login.php
+++ login.php
@@ -20,30 +20,19 @@
     <!-- RIGHT -->
     <div class="auth-right">
         <h2>Login to Your Account</h2>
-
+        <?php if(isset($_GET['error'])){ ?>
+        <p style="color:red;">Email atau password salah!</p>
+        <?php } ?>
         <form action="proses_login.php" method="POST">
-
-            <input type="text" name="username" placeholder="Username" required>
-            <input type="password" name="password" placeholder="Password" required>
-
-            <button type="submit">Login</button>
-
+            <input type="email" name="email" placeholder="Enter your email" required>
+            <input type="password" name="password" placeholder="Enter your password" required>
+            <button type="submit">SIGN IN</button>
         </form>
 
-        <p>Don't have an account? <a href="signup.php">Sign Up</a></p>
+        <p>Don't have an account? <a href="choose_role.php">Sign Up</a></p>
     </div>
 
 </div>
-
-<?php if (isset($_GET['success'])) { ?>
-<div class="popup">
-    <div class="popup-box">
-        <h2>🎉 Login Successful!</h2>
-        <p>Welcome back to Raya Studio</p>
-        <a href="dashboard/customer.php" class="popup-btn">Continue</a>
-    </div>
-</div>
-<?php } ?>
 
 </body>
 </html>
